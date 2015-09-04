95e2f2140bf52447138968dc6463d766aa0ff735
Woody Gilk <woody.gilk@gmail.com>
2015-09-04 09:23:28 -0500

    Enforce utf-8 encoding for strings
    Prevents UnicodeEncodeError from popping up with author names.

--- cabin-porn-it.py
+++ cabin-porn-it.py
@@ -70,9 +70,9 @@ if r.status_code == 200:
   for post in soup.find_all("li", "post"):
     for photo in post.find_all("div", "photo_div") + post.find_all("div", "photoset_div"):
       image = photo.find("img")
-      image_src = str(image.get("src"))
+      image_src = str(image.get("src").encode('utf-8'))
       if re.search("jpg|png", image_src):
-        link_str = str(image.get("title"))
+        link_str = str(image.get("title").encode('utf-8'))
         if link_str == "None":
           link_str = post.find("div", "fb-like").get("data-href")
         cabins.append({ "src" : image_src, "link": link_str })
